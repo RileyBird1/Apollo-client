83c3882682f2cc9d7da6a4c57611aa3e303dec51
Riley B <rbprog68123@gmail.com>
2025-12-19 17:16:37 -0700

    Replace InventoryService with SupplierService

--- src/app/supplier/supplier-create/supplier-create.component.ts
+++ src/app/supplier/supplier-create/supplier-create.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { InventoryService } from '../inventory.service';
+import { SupplierService } from '../supplier.service';
 import { NgIf } from '@angular/common';
 
 @Component({
@@ -50,7 +50,7 @@ export class SupplierCreateComponent {
   submitted = false;
   responseMessage = '';
 
-  constructor(private inventoryService: InventoryService, private fb: FormBuilder, private http: HttpClient) {
+  constructor(private supplierService: SupplierService, private fb: FormBuilder, private http: HttpClient) {
     this.supplierForm = this.fb.group({
       supplierId: ['', [Validators.required, Validators.min(1)]],
       supplierName: ['', Validators.required],
